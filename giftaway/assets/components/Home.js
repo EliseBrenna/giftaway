b73c2a237ea2f670d15d5d92a0ba1b2c3e44fb2c
Therese <brobacktherese@gmail.com>
2020-02-04 13:22:49 +0100

    splashscreen

--- giftaway/assets/components/Home.js
+++ giftaway/assets/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { LinearGradient } from 'expo-linear-gradient';
 import { 
   StyleSheet,
     Text,
@@ -7,6 +8,8 @@ import {
     Image,
     ActivityIndicator,
      } from 'react-native';
+
+import giftaway from '../img/giftaway_logo.png';
 
 export default class Home extends React.Component {
     static navigationOptions = {
@@ -59,10 +62,37 @@ export default class Home extends React.Component {
 
 class SplashScreen extends React.Component {
     render() {
+          const splashStyle = {
+            logo: {
+                width: 150,
+                height:  110,
+                alignItems: 'center',
+                justifyContent: 'center',
+            },
+            container: {
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100vw',
+                height: '100vh'
+            }
+        };
+
         return (
-            <View>
-                <Text>Hei</Text>
+
+            <View style={splashStyle.container}>
+                <LinearGradient
+                    colors={['#f8b195', '#f67280']}
+                    style={splashStyle.container}>
+                    <Image source={{uri: giftaway }} style={splashStyle.logo} />
+                </LinearGradient>   
             </View>
+
+
+    
+                
+
         )
     }
 }
+
