76c58369e93a56d8a5fd1a02f8c6c0edd266d035
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-02-04 13:24:03 +0100

    ..

--- giftaway/assets/components/Home.js
+++ giftaway/assets/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Font from 'expo-font';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { 
   StyleSheet,
@@ -8,15 +9,21 @@ import {
     ActivityIndicator,
      } from 'react-native';
 
+
+import mom from '../img/mom.png';
+import dad from '../img/dad.png';
+import brother from '../img/brother.png';
+import sister from '../img/sister.png';
+import girlfriend from '../img/girlfriend.png';
+import boyfriend from '../img/boyfriend.png';
+
 export default class Home extends React.Component {
     static navigationOptions = {
         title: '',
         headerStyle: {
-            backgroundColor: '#FFF',
             height: 0,
             borderBottomWidth: 0
-        },
-        headerTintColor: '#FFF'
+        }
     };
 
     constructor(props) {
@@ -29,6 +36,7 @@ export default class Home extends React.Component {
     }
 
     async componentDidMount() {
+
         const data = await this.performTimeConsumingTask();
 
         if (data !== null) {
@@ -49,11 +57,62 @@ export default class Home extends React.Component {
         if (this.state.isLoading) {
             return <SplashScreen />;
         } 
+
+        
         return (
-            <View>
-                <Text>Hei</Text>
+            <View style={styles.container}>
+
+                <Text>WHO ARE YOU GETTING A GIFT?</Text>
+
+                <View style={styles.profilesContainer}>
+
+                    <View style={styles.profiles}>
+                        <TouchableOpacity>
+                            <Image source={{uri: mom}} style={styles.profilesImages}/>
+                        </TouchableOpacity>
+                        <Text style={styles.customFont}>Mom</Text>
+                    </View>
+
+                    <View style={styles.profiles}>
+                        <TouchableOpacity>
+                            <Image source={{uri: dad}} style={styles.profilesImages}/>
+                        </TouchableOpacity>
+                        <Text>Dad</Text>
+                    </View>
+
+                    <View style={styles.profiles}>
+                        <TouchableOpacity>
+                            <Image source={{uri: brother}} style={styles.profilesImages}/>
+                        </TouchableOpacity>
+                        <Text>Brother</Text>
+                    </View>
+
+                    <View style={styles.profiles}>
+                        <TouchableOpacity>
+                            <Image source={{uri: sister}} style={styles.profilesImages}/>
+                        </TouchableOpacity>
+                        <Text>Sister</Text>
+                    </View>
+
+                    <View style={styles.profiles}>
+                        <TouchableOpacity>
+                            <Image source={{uri: girlfriend}} style={styles.profilesImages}/>
+                        </TouchableOpacity>
+                        <Text>Girlfriend</Text>
+                    </View>
+
+                    <View style={styles.profiles}>
+                        <TouchableOpacity>
+                            <Image source={{uri: boyfriend}} style={styles.profilesImages}/>
+                        </TouchableOpacity>
+                        <Text>Boyfriend</Text>
+                    </View>
+
+                </View>
+
             </View>
         )
+
     }
 }
 
@@ -66,3 +125,36 @@ class SplashScreen extends React.Component {
         )
     }
 }
+
+/* Styling Home */
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F8B195',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    profilesContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    profiles: {
+        textAlign: 'center',
+        flexBasis: '170px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 25,
+    },
+    profilesImages: {
+        width: 100,
+        height:  100,
+        resizeMode: 'contain',
+    },
+    customFont: {
+        fontFamily: 'Nunito-Regular',
+        fontSize: '14px'
+    }
+  });
